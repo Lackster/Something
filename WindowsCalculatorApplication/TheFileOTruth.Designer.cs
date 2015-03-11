f05d01562c6f001acc26844ba5c5176b25c2ccc4
Jared <proulxj1@outlook.com>
2015-03-11 13:27:13 -0400

    Changes
    Changes I made.

---
+++ WindowsCalculatorApplication/TheFileOTruth.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsCalculatorApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberTwo = new System.Windows.Forms.Button();
+            this.NumberThree = new System.Windows.Forms.Button();
+            this.ButtonFive = new System.Windows.Forms.Button();
+            this.Button6 = new System.Windows.Forms.Button();
+            this.ThSign = new System.Windows.Forms.Button();
+            this.ButtonEight = new System.Windows.Forms.Button();
+            this.ButtonNine = new System.Windows.Forms.Button();
+            this.ClearSign = new System.Windows.Forms.Button();
+            this.EqualSign = new System.Windows.Forms.Button();
+            this.PlusSign = new System.Windows.Forms.Button();
+            this.MinusSign = new System.Windows.Forms.Button();
+            this.MultiplicationSign = new System.Windows.Forms.Button();
+            this.NumberOne = new System.Windows.Forms.Button();
+            this.NumberFour = new System.Windows.Forms.Button();
+            this.ButtonSeven = new System.Windows.Forms.Button();
+            this.DivisionSign = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.ConversionBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // NumberTwo
+            // 
+            this.NumberTwo.Location = new System.Drawing.Point(76, 147);
+            this.NumberTwo.Name = "NumberTwo";
+            this.NumberTwo.Size = new System.Drawing.Size(58, 43);
+            this.NumberTwo.TabIndex = 1;
+            this.NumberTwo.Text = "2";
+            this.NumberTwo.UseVisualStyleBackColor = true;
+            this.NumberTwo.Click += new System.EventHandler(this.NumberTwo_Click);
+            // 
+            // NumberThree
+            // 
+            this.NumberThree.Location = new System.Drawing.Point(137, 147);
+            this.NumberThree.Name = "NumberThree";
+            this.NumberThree.Size = new System.Drawing.Size(58, 43);
+            this.NumberThree.TabIndex = 2;
+            this.NumberThree.Text = "3";
+            this.NumberThree.UseVisualStyleBackColor = true;
+            this.NumberThree.Click += new System.EventHandler(this.NumberThree_Click);
+            // 
+            // ButtonFive
+            // 
+            this.ButtonFive.Location = new System.Drawing.Point(76, 98);
+            this.ButtonFive.Name = "ButtonFive";
+            this.ButtonFive.Size = new System.Drawing.Size(58, 43);
+            this.ButtonFive.TabIndex = 3;
+            this.ButtonFive.Text = "5";
+            this.ButtonFive.UseVisualStyleBackColor = true;
+            this.ButtonFive.Click += new System.EventHandler(this.ButtonFive_Click);
+            // 
+            // Button6
+            // 
+            this.Button6.Location = new System.Drawing.Point(140, 98);
+            this.Button6.Name = "Button6";
+            this.Button6.Size = new System.Drawing.Size(58, 43);
+            this.Button6.TabIndex = 4;
+            this.Button6.Text = "6";
+            this.Button6.UseVisualStyleBackColor = true;
+            this.Button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // ThSign
+            // 
+            this.ThSign.Location = new System.Drawing.Point(204, 98);
+            this.ThSign.Name = "ThSign";
+            this.ThSign.Size = new System.Drawing.Size(58, 43);
+            this.ThSign.TabIndex = 5;
+            this.ThSign.Text = ".";
+            this.ThSign.UseVisualStyleBackColor = true;
+            // 
+            // ButtonEight
+            // 
+            this.ButtonEight.Location = new System.Drawing.Point(76, 49);
+            this.ButtonEight.Name = "ButtonEight";
+            this.ButtonEight.Size = new System.Drawing.Size(58, 43);
+            this.ButtonEight.TabIndex = 6;
+            this.ButtonEight.Text = "8";
+            this.ButtonEight.UseVisualStyleBackColor = true;
+            this.ButtonEight.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // ButtonNine
+            // 
+            this.ButtonNine.Location = new System.Drawing.Point(140, 49);
+            this.ButtonNine.Name = "ButtonNine";
+            this.ButtonNine.Size = new System.Drawing.Size(58, 43);
+            this.ButtonNine.TabIndex = 7;
+            this.ButtonNine.Text = "9";
+            this.ButtonNine.UseVisualStyleBackColor = true;
+            this.ButtonNine.Click += new System.EventHandler(this.ButtonNine_Click);
+            // 
+            // ClearSign
+            // 
+            this.ClearSign.Location = new System.Drawing.Point(204, 49);
+            this.ClearSign.Name = "ClearSign";
+            this.ClearSign.Size = new System.Drawing.Size(58, 43);
+            this.ClearSign.TabIndex = 8;
+            this.ClearSign.Text = "Clear";
+            this.ClearSign.UseVisualStyleBackColor = true;
+            this.ClearSign.Click += new System.EventHandler(this.ClearSign_Click);
+            // 
+            // EqualSign
+            // 
+            this.EqualSign.Location = new System.Drawing.Point(201, 147);
+            this.EqualSign.Name = "EqualSign";
+            this.EqualSign.Size = new System.Drawing.Size(61, 43);
+            this.EqualSign.TabIndex = 9;
+            this.EqualSign.Text = "=";
+            this.EqualSign.UseVisualStyleBackColor = true;
+            // 
+            // PlusSign
+            // 
+            this.PlusSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlusSign.Location = new System.Drawing.Point(282, 147);
+            this.PlusSign.Name = "PlusSign";
+            this.PlusSign.Size = new System.Drawing.Size(61, 43);
+            this.PlusSign.TabIndex = 10;
+            this.PlusSign.Text = "+";
+            this.PlusSign.UseVisualStyleBackColor = true;
+            this.PlusSign.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // MinusSign
+            // 
+            this.MinusSign.Location = new System.Drawing.Point(282, 98);
+            this.MinusSign.Name = "MinusSign";
+            this.MinusSign.Size = new System.Drawing.Size(61, 43);
+            this.MinusSign.TabIndex = 11;
+            this.MinusSign.Text = "-";
+            this.MinusSign.UseVisualStyleBackColor = true;
+            this.MinusSign.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // MultiplicationSign
+            // 
+            this.MultiplicationSign.Location = new System.Drawing.Point(282, 49);
+            this.MultiplicationSign.Name = "MultiplicationSign";
+            this.MultiplicationSign.Size = new System.Drawing.Size(61, 43);
+            this.MultiplicationSign.TabIndex = 12;
+            this.MultiplicationSign.Text = "X";
+            this.MultiplicationSign.UseVisualStyleBackColor = true;
+            this.MultiplicationSign.Click += new System.EventHandler(this.button11_Click_1);
+            // 
+            // NumberOne
+            // 
+            this.NumberOne.Location = new System.Drawing.Point(12, 147);
+            this.NumberOne.Name = "NumberOne";
+            this.NumberOne.Size = new System.Drawing.Size(58, 43);
+            this.NumberOne.TabIndex = 13;
+            this.NumberOne.Text = "1";
+            this.NumberOne.UseVisualStyleBackColor = true;
+            this.NumberOne.Click += new System.EventHandler(this.NumberOne_Click);
+            // 
+            // NumberFour
+            // 
+            this.NumberFour.Location = new System.Drawing.Point(12, 98);
+            this.NumberFour.Name = "NumberFour";
+            this.NumberFour.Size = new System.Drawing.Size(58, 43);
+            this.NumberFour.TabIndex = 14;
+            this.NumberFour.Text = "4";
+            this.NumberFour.UseVisualStyleBackColor = true;
+            this.NumberFour.Click += new System.EventHandler(this.NumberFour_Click);
+            // 
+            // ButtonSeven
+            // 
+            this.ButtonSeven.Location = new System.Drawing.Point(12, 49);
+            this.ButtonSeven.Name = "ButtonSeven";
+            this.ButtonSeven.Size = new System.Drawing.Size(58, 43);
+            this.ButtonSeven.TabIndex = 15;
+            this.ButtonSeven.Text = "7";
+            this.ButtonSeven.UseVisualStyleBackColor = true;
+            this.ButtonSeven.Click += new System.EventHandler(this.ButtonSeven_Click);
+            // 
+            // DivisionSign
+            // 
+            this.DivisionSign.Location = new System.Drawing.Point(282, 0);
+            this.DivisionSign.Name = "DivisionSign";
+            this.DivisionSign.Size = new System.Drawing.Size(58, 43);
+            this.DivisionSign.TabIndex = 16;
+            this.DivisionSign.Text = "/";
+            this.DivisionSign.UseVisualStyleBackColor = true;
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(12, 12);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(250, 31);
+            this.text.TabIndex = 18;
+            this.text.TextChanged += new System.EventHandler(this.text_TextChanged);
+            // 
+            // ConversionBox
+            // 
+            this.ConversionBox.FormattingEnabled = true;
+            this.ConversionBox.Location = new System.Drawing.Point(366, 12);
+            this.ConversionBox.Name = "ConversionBox";
+            this.ConversionBox.Size = new System.Drawing.Size(121, 21);
+            this.ConversionBox.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 210);
+            this.Controls.Add(this.ConversionBox);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.DivisionSign);
+            this.Controls.Add(this.ButtonSeven);
+            this.Controls.Add(this.NumberFour);
+            this.Controls.Add(this.NumberOne);
+            this.Controls.Add(this.MultiplicationSign);
+            this.Controls.Add(this.MinusSign);
+            this.Controls.Add(this.PlusSign);
+            this.Controls.Add(this.EqualSign);
+            this.Controls.Add(this.ClearSign);
+            this.Controls.Add(this.ButtonNine);
+            this.Controls.Add(this.ButtonEight);
+            this.Controls.Add(this.ThSign);
+            this.Controls.Add(this.Button6);
+            this.Controls.Add(this.ButtonFive);
+            this.Controls.Add(this.NumberThree);
+            this.Controls.Add(this.NumberTwo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NumberTwo;
+        private System.Windows.Forms.Button NumberThree;
+        private System.Windows.Forms.Button ButtonFive;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Button ThSign;
+        private System.Windows.Forms.Button ButtonEight;
+        private System.Windows.Forms.Button ButtonNine;
+        private System.Windows.Forms.Button ClearSign;
+        private System.Windows.Forms.Button EqualSign;
+        private System.Windows.Forms.Button PlusSign;
+        private System.Windows.Forms.Button MinusSign;
+        private System.Windows.Forms.Button MultiplicationSign;
+        private System.Windows.Forms.Button NumberOne;
+        private System.Windows.Forms.Button NumberFour;
+        private System.Windows.Forms.Button ButtonSeven;
+        private System.Windows.Forms.Button DivisionSign;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.ComboBox ConversionBox;
+    }
+}
+
---
+++ WindowsCalculatorApplication/TheFileOTruth.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsCalculatorApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string contents;
+        public string lastSign;
+        int swapper;
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void NumberThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberOne_Click(object sender, EventArgs e)
+        {
+            if (contents == null)
+            {
+                try
+                {
+                    int switchCase = 1;
+                    if (lastSign != null)
+                    {
+                        if (lastSign == "+")
+                        {
+                            switchCase = 1;
+                        }else if(lastSign == "-")
+                        {
+                            switchCase = 2;
+                        }
+                        else if (lastSign == "*")
+                        {
+                            switchCase = 3;
+                        }else if (lastSign == "/")
+                        {
+                            switchCase = 4;
+                        }
+                        else { MessageBox.Show("Error with NumberOne sign switch case. Please report to creator!"); }
+                        
+                        switch (switchCase)
+                        {
+                                
+                            case 1:
+                                swapper = 0;
+                                swapper = swapper + 1;
+                                contents = swapper.ToString();
+                                lastSign = null;
+                                break;
+
+                            case 2:
+                                swapper = 0;
+                                swapper = swapper - 1;
+                                contents = swapper.ToString();
+                                lastSign = null;
+                                break;
+
+                            case 3:
+                                swapper = 0;
+                                swapper = swapper * 1; // I realize that this gives me 0, but who cares. Might as well.
+                                contents = swapper.ToString();
+                                lastSign = null;
+                                break;
+                            case 4:
+                                MessageBox.Show("You cannot divide by zero. Sorry!");
+                                lastSign = null;
+                                break;
+                        }
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                if (lastSign == null)
+                {
+
+                }
+            }
+int oneMod = Int32.Parse(contents);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonFive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberFour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonNine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSeven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearSign_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+            text.Text = contents;
+        }
+    }
+}
